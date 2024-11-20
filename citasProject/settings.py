412aa875fdf9dc6d62b85a95562341574fdf388a
javier Rival Llanquilef <llanquilef.cl@gmail.com>
2024-11-20 10:46:14 -0300

    cambie la base de datos a postgre en clase para demostración capaz la cambié a mysql

--- citasProject/settings.py
+++ citasProject/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'citasProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dbCitas',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
 
